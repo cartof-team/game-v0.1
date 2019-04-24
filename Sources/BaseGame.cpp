91eca06df30c0d8f1d26ac604432e334ef00ec2c
Alpha696 <sarofincristian@gmail.com>
2019-04-24 12:49:02 +0300

    Add files via upload

---
+++ Sources/BaseGame.cpp
@@ -0,0 +1,62 @@
+#include "BaseGame.h"
+
+BaseGame::BaseGame()
+{
+app_window.create(sf::VideoMode(WindowX,WindowY), "The Game");
+rect.setSize(sf::Vector2f(40,50));
+rect.setFillColor(sf::Color::Blue);
+}
+
+BaseGame::~BaseGame()
+{
+    //dtor
+}
+
+void BaseGame::MainLoop()
+{
+    while (app_window.isOpen())
+    {
+        sf::Event event;
+        while (app_window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                GameState=0;
+        }
+        if(GameState==0)
+            app_window.close();
+        app_window.clear(sf::Color::Black);
+        renderAllObjs();
+        app_window.display();
+    }
+}
+
+
+void BaseGame::render(sf::RenderTarget& window, sf::RectangleShape Shape)
+{
+    window.draw(Shape);
+}
+
+void BaseGame::render(sf::RenderTarget& window, sf::CircleShape Shape)
+{
+    window.draw(Shape);
+}
+
+void BaseGame::render(sf::RenderTarget& window, sf::Sprite Shape)
+{
+    window.draw(Shape);
+}
+
+void BaseGame::render(sf::RenderTarget& window, sf::ConvexShape Shape)
+{
+    window.draw(Shape);
+}
+
+void BaseGame::renderAllObjs()
+{
+    render(app_window, rect);
+}
+
+sf::RectangleShape& BaseGame::getRectangle()
+{
+    return rect;
+}
