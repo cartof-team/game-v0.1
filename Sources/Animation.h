68fd31b3ab616587f676ee78fa3a250cf59d7e3e
Alpha696 <sarofincristian@gmail.com>
2019-04-25 23:26:39 +0300

    Update Animation.h

--- Sources/Animation.h
+++ Sources/Animation.h
@@ -18,3 +18,14 @@ class Animation
 };
 
 #endif // ANIMATION_H
+
+/*
+cand faci o animatie creezi un object nou pt fiecare animatie, cu textura, (vector2u)numarul de coloane si linii ale animatie si timpul
+in secunde intre animatii (de preferat 0.3 sau 0.1)
+ex: Animation anim1(&AnimTXT1, sf::Vector2u(10,4), 0.1);
+
+dupa folosesti functia Update ca sa treaca la urmatorul frame. Ca parametrii setezi coloana si deltaTime-ul
+ex: anim1.Update(0, deltaTime);
+//0 pt prima linie
+  1 pt a doua etcc
+*/
